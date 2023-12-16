c2bd12da86a57703478fdf7ea59b66e49ebb2bb1
rathoreumesh90 <137924394+rathoreumesh90@users.noreply.github.com>
2023-12-16 20:39:26 +0530

    Create name-var-demo.sh

---
+++ name-var-demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Script to show how to use variables
+#
+#
+#
+#
+#
+name="Radhe"
+echo "My name is $name"
+#
+#
+#
+#
+#Var to sto the output of a command
+HOSTNAME=$(hostname)
+echo "name of this maschine is $HOSTNAME"
+
